20ba27fac944aba8962ca075776599d956844816
Lauren <105395021+Laatinah@users.noreply.github.com>
2022-06-22 10:30:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.alert("Name: Lauren Adjei-Tawiah\nHeight: 1.5m\nCountry: Swaziland (temporary residence)");
+
+function me() {
+  alert("Name: Lauren Adjei-Tawiah\n Height: 1.5m\n Country: Swaziland (temporary residence)");
+}
